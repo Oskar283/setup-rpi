6dd81b40c51d2868185023d7eb7d2f4de2ba0c3d
Oskar <oskarrodholm@msn.com>
2021-03-01 19:00:57 +0000

    Add automatic pull of dotfiles

--- install_programs.sh
+++ install_programs.sh
@@ -3,4 +3,13 @@
 # Installs latest of all as I do not need HA.
 
 
-apt -y install tmux vim git
+apt -y install tmux vim git cmake
+
+if [ -d "~/dotfiles"]
+then
+    git clone https://github.com/Oskar283/dotfiles.git ~/dotfiles
+else
+    echo "dotfiles already cloned"
+fi
+
+/bin/bash /home/$USER/dotfiles/install
